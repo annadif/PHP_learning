435e0114ef5fb194da3c358fd04b32a91adb07e4
annadif <128553523+annadif@users.noreply.github.com>
2025-06-21 01:51:19 +0300

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,73 @@
+<?php
+    require 'database.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Burger Code</title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+        <link href='http://fonts.googleapis.com/css?family=Holtwood+One+SC' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+        <link rel="stylesheet" href="../css/styles.css">
+    </head>
+    
+    <body>
+        <h1 class="text-logo"><span class="bi-shop"></span> Burger Code <span class="bi-shop"></span></h1>
+        <div class="container admin">
+            <div class="row">
+                <h1><strong>Ajouter un item</strong></h1>
+                <br>
+                <form class="form" action="insert.php" role="form" method="post" enctype="multipart/form-data">
+                    <br>
+                    <div>
+                        <label class="form-label" for="name">Nom:</label>
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Nom" value="<?php echo $name;?>">
+                        <span class="help-inline"><?php echo $nameError;?></span>
+                    </div>
+                    <br>
+                    <div>
+                        <label class="form-label" for="description">Description:</label>
+                        <input type="text" class="form-control" id="description" name="description" placeholder="Description" value="<?php echo $description;?>">
+                        <span class="help-inline"><?php echo $descriptionError;?></span>
+                    </div>
+                    <br>
+                    <div>
+                        <label class="form-label" for="price">Prix: (en €)</label>
+                        <input type="number" step="0.01" class="form-control" id="price" name="price" placeholder="Prix" value="<?php echo $price;?>">
+                        <span class="help-inline"><?php echo $priceError;?></span>
+                    </div>
+                    <br>
+                    <div>
+                        <label class="form-label" for="category">Catégorie:</label>
+                        <select class="form-control" id="category" name="category">
+                        <?php
+                           $db = Database::connect();
+                           foreach ($db->query('SELECT * FROM categories') as $row) {
+                                echo '<option value="'. $row['id'] .'">'. $row['name'] . '</option>';;
+                           }
+                           Database::disconnect();
+                        ?>
+                        </select>
+                        <span class="help-inline"><?php echo $categoryError;?></span>
+                    </div>
+                    <br>
+                    <div>
+                        <label class="form-label" for="image">Sélectionner une image:</label>
+                        <input type="file" id="image" name="image"> 
+                        <span class="help-inline"><?php echo $imageError;?></span>
+                    </div>
+                    <br>
+                    <div class="form-actions">
+                        <button type="submit" class="btn btn-success"><span class="bi-pencil"></span> Ajouter</button>
+                        <a class="btn btn-primary" href="index.php"><span class="bi-arrow-left"></span> Retour</a>
+                   </div>
+                </form>
+            </div>
+        </div>   
+    </body>
+</html>
